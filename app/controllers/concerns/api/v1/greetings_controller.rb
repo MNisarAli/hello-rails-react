abbce415ed362c622d8663688b9176f7f74dd61f
MNisarAli <dr.nisaralig@gmail.com>
2024-02-17 10:26:43 +0500

    Update greetings_controller index action

--- app/controllers/concerns/api/v1/greetings_controller.rb
+++ app/controllers/concerns/api/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
     greeting = Message.order('RANDOM()').first
-    render json: { greeting: greeting.content }
+    render json: { message: greeting.content }
   end
 end
